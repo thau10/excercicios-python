5e953f52d82e87242ada14e7a286bf3ee320a5bf
Thauane Lourenco <128156748+thau10@users.noreply.github.com>
2024-03-11 09:04:39 -0300

    Add files via upload

---
+++ Votar.js
@@ -0,0 +1,15 @@
+let idade = 25;
+let cidadania = true
+
+// Verificar se a idade é maior ou igual a 18 e se a cidadania é brasileira (true)
+
+if (idade >18 && possuiCarteiraDeMotorista){
+    console.log("pode dirigir");
+} else{
+    console.log("Não pode dirigir");
+// Variáveis representando idade e cidadania
+
+
+}
+//Valida
+
